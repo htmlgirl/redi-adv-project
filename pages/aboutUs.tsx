a6b3527a36c39863f793023abb3c0488e9a1664f
Tetiana Luskatova <t2.luskatova@gmail.com>
2024-12-04 20:23:49 +0100

    text size fix

--- pages/aboutUs.tsx
+++ pages/aboutUs.tsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
             <Header/>
             <div className="flex items-center justify-center min-h-screen bg-gray-100">
                 <div className="flex flex-row items-start gap-4 max-w-[1800px] w-full">
-                    <div className="text-blue-500 text-sm">
+                    <div className="text-blue-500 text-2xl">
                         Hello everyone. This is a library of films that you can add to yourself.
                         You just need to fill out the form on the Home page and add the desired film to the list.
                     </div>
